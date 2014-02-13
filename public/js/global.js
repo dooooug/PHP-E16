134afc15614e46711700e413ec914b890043ef58
Kévin <kevinchassagne@gmail.com>
2014-02-13 11:28:26 +0100

    front aperçu

---
+++ public/js/global.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+/* WINDOW HEIGHT */
+
+/*
+resizeContent();
+	
+$(window).resize(function() {
+        resizeContent();
+    });
+
+function resizeContent() {
+    $height = $(window).height();
+    $('.rightContent').height($height);
+}
+*/
+
+/* DATE */
+var date  = new Date;
+var moi   = date.getMonth();
+var mois  = new Array('Janvier', 'F&eacute;vrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Ao&ucirc;t', 'Septembre', 'Octobre', 'Novembre', 'D&eacute;cembre');
+var j     = date.getDate();
+var jour  = date.getDay();
+var jours = new Array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+var date  = jours[jour]+' <span>'+j+'</span> '+mois[moi];
+
+$('.leftContent time').html(date);
+
+/* DAY ACTIVE */
+var day = jours[jour].toLowerCase();
+$('.' + day).addClass('selected');
+
+$('html, body').animate({
+	scrollTop: $('.' + day).offset().top
+}, 1000);
+
+
+});
