0c684531c14764e289b7a792065fd08fa00360f0
Quentin Morisseau <quentin.morisseau@gmail.com>
2014-03-07 03:33:18 +0100

    Edit d'évent

--- App/public/js/script.js
+++ App/public/js/script.js
@@ -5,7 +5,6 @@ $('a.edit').on('click',function(e){
     method:'get'
 	})
 	.success(function(data){
-    alert(data);
 		$("section#popin div#edit").html(data);
     $("section#popin").toggle();
 	});
